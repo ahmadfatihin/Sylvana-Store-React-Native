0f2ffd9db56271033ee79bec4263ffe993260746
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-09-23 14:05:00 +0700

    debug metro config

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,17 @@
-// expo v40:
-const { getDefaultConfig } = require("@expo/metro-config");
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-// expo v41: 
-// remove the @ (see: https://blog.expo.io/expo-sdk-41-12cc5232f2ef)
-const { getDefaultConfig } = require("expo/metro-config");
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig(__dirname);
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
+ module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};

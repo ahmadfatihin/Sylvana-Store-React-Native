5a77aa9a5a53c996a79f3076916dd09f89d0da8f
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-09-27 17:17:46 +0700

    Fix React Navigation
    Fix Error in navigation, add router index

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from './pages/Home';
-import Splash from './pages/Splash';
-
-const Stack = createNativeStackNavigator();
+import Router from './router';
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen
-          name="Splash"
-          component={Splash}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+      <Router />
     </NavigationContainer>
   );
 }
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home, Splash} from '../pages';
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;

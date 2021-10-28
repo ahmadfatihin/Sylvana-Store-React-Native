1d66b189d34505d741a9cf2b402db4fa2aa515db
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-10-28 11:38:19 +0700

    Add Another Icon
    Upgrade Index.js on Assets/Icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,37 @@
 import IconHomeAktif from './home-aktif.svg';
 import IconHome from './home.svg';
+import IconBajuAktif from './baju-aktif.svg';
+import IconBaju from './baju.svg';
+import IconProfileAktif from './profile-aktif.svg';
+import IconProfile from './profile.svg';
+import IconCari from './search.svg';
+import IconKeranjang from './shopping-cart.svg';
+import IconArrowRight from './arrow-right.svg';
+import IconChangePassword from './change-password.svg';
+import IconEditProfile from './user-edit.svg';
+import IconSignOut from './sign-out.svg';
+import IconHistory from './history.svg';
+import IconBack from './arrow-left.svg';
+import IconKeranjangPutih from './keranjang-putih.svg';
+import IconHapus from './hapus.svg';
+import IconSubmit from './submit.svg';
 
-export {IconHomeAktif, IconHome};
+export {
+  IconHome,
+  IconHomeAktif,
+  IconBaju,
+  IconBajuAktif,
+  IconProfile,
+  IconProfileAktif,
+  IconCari,
+  IconKeranjang,
+  IconArrowRight,
+  IconChangePassword,
+  IconEditProfile,
+  IconHistory,
+  IconSignOut,
+  IconBack,
+  IconKeranjangPutih,
+  IconHapus,
+  IconSubmit,
+};

52837c8243a505486c6ca4261b6b7f3cb274a495
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-11-09 16:27:01 +0700

    Set BottomNavbar Absolute

--- src/components/besar/BottomNavigator/index.js
+++ src/components/besar/BottomNavigator/index.js
@@ -54,6 +54,10 @@ export default BottomNavigator;
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
     backgroundColor: colors.primary,
     paddingVertical: 8,
     paddingHorizontal: 35,

18b41db9fe41e23b326f140d7f97bba2a7c837bc
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-11-10 17:51:28 +0700

    Create Styling for Homepages
    Avoid Hardcoding Bro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import {HeaderComponent} from '../../components';
+import {colors} from '../../utils';
 
 export default class Home extends Component {
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={styles.page}>
         <HeaderComponent />
         <Text>Home Screen</Text>
       </View>
@@ -13,4 +14,6 @@ export default class Home extends Component {
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {flex: 1, backgroundColor: colors.white},
+});

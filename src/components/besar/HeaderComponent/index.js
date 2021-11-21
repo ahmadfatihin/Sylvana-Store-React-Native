b91910890c5acae518fb3e7046ffaf556dc2c189
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-11-21 23:33:23 +0700

    use Responsive Height for Header

--- src/components/besar/HeaderComponent/index.js
+++ src/components/besar/HeaderComponent/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
-import {colors} from './../../../utils';
+import {colors, responsiveHeight} from './../../../utils';
 
 export default class HeaderComponent extends Component {
   render() {
@@ -13,5 +13,5 @@ export default class HeaderComponent extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {backgroundColor: colors.primary, height: 125},
+  container: {backgroundColor: colors.primary, height: responsiveHeight(125)},
 });

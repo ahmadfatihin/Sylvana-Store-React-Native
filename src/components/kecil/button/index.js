2e790c769202b7b7f752143537daacda9ee1d350
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-11-29 23:58:35 +0700

    Update Button

--- src/components/kecil/button/index.js
+++ src/components/kecil/button/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 
 const Button = () => {
   return (
-    <View>
+    <TouchableOpacity>
       <Text></Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
